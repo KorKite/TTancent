0ee2c549f25c2dffc9d3a4020376015dece1843a
고준서 <sta06167@naver.com>
2021-08-08 23:27:20 +0900

    client login api modify

---
+++ client/database/login.py
@@ -0,0 +1,12 @@
+import requests
+
+def login_valid(useremail, password):
+    form = requests.get(f"http://junxxuh.gabia.io/clientvalid/{useremail}/{password}").json()
+    return form 
+
+
+if __name__ == "__main__":
+    print(login_valid("test@test", "test")) # Sucess Case
+    print(login_valid("ttt@ttt", "ttt")) # False Case1 -> No Email
+    print(login_valid("test@test", "ttt")) # False Case2 -> Password Wrong
+    print(login_valid(None, None)) # False Case2 -> Password Wrong
